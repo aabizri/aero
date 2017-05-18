466c5865e6c0d8c8fb4eaafaee0d7fd0233332d4
Alexandre A. Bizri <alexandre@bizri.fr>
2017-05-18 15:06:37 +0200

    adexp: Add architecture documentation for adexp subpackage

--- adexp/adexp.go
+++ adexp/adexp.go
@@ -1,6 +1,12 @@
 /*
 Package adexp provides marshalling and unmarshalling of ADEXP documents.
 See: http://www.eurocontrol.int/sites/default/files/publication/files/20111001-adexp-spec-v3.1.pdf
+
+Architecture:
+	lexer provides the definition of a ADEXP lexer (DONE)
+	parser provides the definition of a ADEXP parser (WIP)
+	serialiser provides the definition of an ADEXP serialiser (TODO)
+	this package wraps it all together so as to provide easy & simple unmarshalling of ADEXP documents (WIP)
 */
 package adexp
 

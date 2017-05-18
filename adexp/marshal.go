86269483567f99b99095b57c64d675486ce2c5fb
Alexandre A. Bizri <alexandre@bizri.fr>
2017-05-18 16:29:41 +0200

    adexp: Improve Encoder setup

--- adexp/marshal.go
+++ adexp/marshal.go
@@ -3,8 +3,6 @@ package adexp
 import (
 	"bytes"
 	"io"
-
-	"github.com/pkg/errors"
 )
 
 // MarshalText marshals an ADEXP message to string
@@ -34,34 +32,19 @@ func NewEncoder(w io.Writer) *Encoder {
 
 // SetIndent sets the indentation. If indent is "", then no indentation will be applied.
 // The default indentation is \t. If you want tabs as a separator then you might use SetSeparator.
-func SetIndent(indent string) func(*Encoder) error {
-	return func(e *Encoder) error {
-		e.indent = indent
-		return nil
-	}
+func (enc *Encoder) SetIndent(indent string) error {
+	enc.indent = indent
+	return nil
 }
 
 // SetSep sets the separator. There has to be at least one width of separator.
-func SetSep(sep string) func(*Encoder) error {
-	return func(e *Encoder) error {
-		e.sep = sep
-		return nil
-	}
-}
-
-// Set set the given options
-func (e *Encoder) Set(setters ...(func(*Encoder) error)) error {
-	for i, s := range setters {
-		err := s(e)
-		if err != nil {
-			return errors.Wrapf(err, "Set: error while setting value #%d", i)
-		}
-	}
+func (enc *Encoder) SetSep(sep string) error {
+	enc.sep = sep
 	return nil
 }
 
 // Encode encodes a given ADEXP message
-func (e *Encoder) Encode(msg ADEXP) error {
+func (enc *Encoder) Encode(msg ADEXP) error {
 	// DO SOMETHING
 	return nil
 }

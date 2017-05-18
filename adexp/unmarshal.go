e372b913ba923c2032f3603b09465db2d1c55535
Alexandre A. Bizri <alexandre@bizri.fr>
2017-05-18 16:30:12 +0200

    adexp: Improve Decoder setup

--- adexp/unmarshal.go
+++ adexp/unmarshal.go
@@ -3,6 +3,8 @@ package adexp
 import (
 	"bytes"
 	"io"
+
+	"github.com/aabizri/aero/adexp/parser"
 )
 
 // UnmarshalText unmarshals the given text to msg.
@@ -16,7 +18,8 @@ func (msg ADEXP) UnmarshalText(text []byte) error {
 
 // A Decoder decodes an input stream to an ADEXP map
 type Decoder struct {
-	reader io.Reader
+	reader     io.Reader
+	parserFunc func(io.Reader) parser.Parser
 }
 
 // NewDecoder returns a default Decoder.
@@ -24,6 +27,13 @@ func NewDecoder(r io.Reader) *Decoder {
 	return &Decoder{
 		reader: r,
 	}
+}
+
+// SetParser sets the Parser-obtaining function to be used.
+// If used after the first call to Decode, this results in an error.
+func (dec *Decoder) SetParser(new func(io.Reader) parser.Parser) error {
+	dec.parserFunc = new
+	return nil
 }
 
 // Decode decodes the input stream to the given ADEXP msg
